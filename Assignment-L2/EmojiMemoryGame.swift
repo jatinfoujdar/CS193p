179f69378700ed14c8b1b6e1f87d4853862fab23
Jatin Foujdar <jatin321001@gmail.com>
2024-09-14 11:42:11 +0530

    class commit

---
+++ Assignment-L2/EmojiMemoryGame.swift
@@ -0,0 +1,12 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by jatin foujdar on 14/09/24.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame: {
+    var model: MemorizeGameModel<String>
+}
